02449db2cebb0a52ed7fabacaa53a4af9ea3d41b
arthgirard <mailarthurgirard@gmail.com>
2026-08-15 13:51:13 -0400

    v1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.monolith.app"
         minSdk = 26
         targetSdk = 34
-        versionCode = 12
-        versionName = "1.2.0"
+        versionCode = 13
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
